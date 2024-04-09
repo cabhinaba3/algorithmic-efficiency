b240ee3495fbb91dfaaee9f642007d07bd438007
Priya Kasimbeg <kasimbeg@google.com>
2024-04-09 20:03:13 +0000

    fix run commands

--- utils/run.sh
+++ utils/run.sh
@@ -1,26 +1,27 @@
 python run_workloads.py \
 --framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
---workload_config_path submission_configs_small/AlgoPerf_Team_1_self_tuning_AdamG.json
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_1_self_tuning_AdamG.json \
+--dry_run
 
 python run_workloads.py \
 --framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
 --workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_cyclic_lr.json
 
 python run_workloads.py \
 --framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
 --workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_ema_cyclic_lr.json
 
 python run_workloads.py \
 --framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
 --workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_queue.json
 
 python run_workloads.py \
 --framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
 --workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_ema.json
 
 python run_workloads.py \
