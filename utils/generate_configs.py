cf9d9c9ba7f109564812743d13de655147ee85f5
Priya Kasimbeg <kasimbeg@google.com>
2024-04-09 03:11:55 +0000

    add configs for checks

---
+++ utils/generate_configs.py
@@ -0,0 +1,38 @@
+import os
+import pprint
+import copy
+import json
+
+SUBMISSON_DIR = "/home/kasimbeg/submissions_algorithms_v0.5"
+WORKLOADS_BASE_CONFIG_PATH = "/home/kasimbeg/algorithmic-efficiency/utils/workload_config_v05_base_small.json"
+
+with open(WORKLOADS_BASE_CONFIG_PATH) as f:
+    base_workload_config = json.load(f)
+
+submissions = {}
+
+for root, dirs, files in os.walk(SUBMISSON_DIR):
+    if 'submission.py' in files:
+        submission_path = os.path.join(root, 'submission.py')
+        submission_name = '_'.join(submission_path.split('/')[4:-1])
+        print(submission_name)
+
+        submission_dict = {}
+        submission_dict['submission_path'] = submission_path
+        if 'external_tuning' in submission_path:
+            submission_dict['tuning_search_space'] = os.path.join(root, 'tuning_search_space.json')
+        if 'requirements.txt' in os.listdir(root):
+            submission_dict['requirements'] = os.path.join(root, 'requirements.txt')
+        submissions[submission_name] = submission_dict
+
+
+for submission_name, submission_info in submissions.items():
+    workload_config = copy.deepcopy(base_workload_config)
+    for workload in workload_config.keys():
+        workload_config[workload]['submission_path'] = submission_info['submission_path']   
+        if 'tuning_search_space' in submission_info.keys():
+            workload_config[workload]['tuning_search_space'] = submission_info['tuning_search_space']
+    
+    with open(os.path.join('submission_configs_small', f'{submission_name}.json'), 'w',) as f:
+        json.dump(workload_config, f, indent=4)
+
---
+++ utils/run.sh
@@ -0,0 +1,44 @@
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_1_self_tuning_AdamG.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_cyclic_lr.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_ema_cyclic_lr.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_queue.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_25_external_tuning_lawa_ema.json
+
+python run_workloads.py \
+--framework jax \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_3_external_tuning_amos.json
+
+python run_workloads.py \
+--framework jax \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_11_external_tuning_nadamp.json
+
+python run_workloads.py \
+--framework jax \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_11_external_tuning_generalized_adam.json
+
+python run_workloads.py \
+--framework jax \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_11_self_tuning.json
