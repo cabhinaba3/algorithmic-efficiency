86c3fd5e09977dc7875a52d3185ecd8210bb2632
Priya Kasimbeg <kasimbeg@google.com>
2024-04-09 22:22:58 +0000

    update configs

--- utils/generate_configs.py
+++ utils/generate_configs.py
@@ -27,9 +27,8 @@ for root, dirs, files in os.walk(SUBMISSON_DIR):
             submission_dict['tuning_ruleset'] = 'external'
         else:
             submission_dict['tuning_ruleset'] = 'self'
-            
         if 'requirements.txt' in os.listdir(root):
-            submission_dict['requirements'] = os.path.join(rel_root, 'requirements.txt').replace('0.5', '0_5')
+            submission_dict['requirements_path'] = os.path.join(rel_root, 'requirements.txt').replace('0.5', '0_5')
         submissions[submission_name] = submission_dict
 
 
@@ -39,6 +38,9 @@ for submission_name, submission_info in submissions.items():
         workload_config[workload]['submission_path'] = submission_info['submission_path']   
         if 'tuning_search_space' in submission_info.keys():
             workload_config[workload]['tuning_search_space'] = submission_info['tuning_search_space']
+        if 'requirements_path' in submission_info.keys():
+            workload_config[workload]['requirements_path'] = submission_info['requirements_path']
+        workload_config[workload]['tuning_ruleset'] = submission_info['tuning_ruleset']
     
     with open(os.path.join('submission_configs_small', f'{submission_name}.json'), 'w',) as f:
         json.dump(workload_config, f, indent=4)
