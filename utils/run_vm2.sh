0577a3328b550e0670d2d007b8eab43a7a5bb55b
Priya Kasimbeg <kasimbeg@google.com>
2024-04-10 19:30:33 +0000

    add script to run next batch

---
+++ utils/run_vm2.sh
@@ -0,0 +1,35 @@
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_adamw.json \
+--dry_run
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_prodigy.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_10_self_tuning_schedule_free_adamw.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_17_self_tuning_lwrkap.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_21_external_tuning_shampoo_submission.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_6_external_tuning_ggT.json
+
+python run_workloads.py \
+--framework jax \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_16_external_tuning_caspr_adaptive.json
