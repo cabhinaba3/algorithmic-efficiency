591aba95cd689968e99736cc9110d3840d037f69
Priya Kasimbeg <kasimbeg@google.com>
2024-04-11 23:38:29 +0000

    fix

--- utils/run_vm2.sh
+++ utils/run_vm2.sh
@@ -18,10 +18,10 @@
 # --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
 # --workload_config_path submission_configs_small/AlgoPerf_Team_21_external_tuning_shampoo_submission.json
 
-# python run_workloads.py \
-# --framework pytorch \
-# --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
-# --workload_config_path submission_configs_small/AlgoPerf_Team_6_external_tuning_ggT.json
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_6_external_tuning_ggT.json
 
 python run_workloads.py \
 --framework jax \
