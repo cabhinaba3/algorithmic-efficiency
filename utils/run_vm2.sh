aec4ce066feecceea8137ef4cd9c793a4b008771
Priya Kasimbeg <kasimbeg@google.com>
2024-04-11 18:00:37 +0000

    modify run file

--- utils/run_vm2.sh
+++ utils/run_vm2.sh
@@ -1,22 +1,17 @@
-python run_workloads.py \
---framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
---workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_adamw.json \
+# python run_workloads.py \
+# --framework pytorch \
+# --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+# --workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_adamw.json \
 
-python run_workloads.py \
---framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
---workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_prodigy.json
+# python run_workloads.py \
+# --framework pytorch \
+# --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+# --workload_config_path submission_configs_small/AlgoPerf_Team_10_external_tuning_schedule_free_prodigy.json
 
-python run_workloads.py \
---framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
---workload_config_path submission_configs_small/AlgoPerf_Team_10_self_tuning_schedule_free_adamw.json
-
-python run_workloads.py \
---framework pytorch \
---docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
---workload_config_path submission_configs_small/AlgoPerf_Team_17_self_tuning_lwrkap.json
+# python run_workloads.py \
+# --framework pytorch \
+# --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+# --workload_config_path submission_configs_small/AlgoPerf_Team_10_self_tuning_schedule_free_adamw.json
 
 python run_workloads.py \
 --framework pytorch \
@@ -32,3 +27,8 @@ python run_workloads.py \
 --framework jax \
 --docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_jax_dev \
 --workload_config_path submission_configs_small/AlgoPerf_Team_16_external_tuning_caspr_adaptive.json
+
+python run_workloads.py \
+--framework pytorch \
+--docker_image_url us-central1-docker.pkg.dev/training-algorithms-external/mlcommons-docker-repo/algoperf_pytorch_dev \
+--workload_config_path submission_configs_small/AlgoPerf_Team_17_self_tuning_lwrkap.json
